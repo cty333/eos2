57ca8f19dede6c20f064036a5b39f284f199410b
cty333 <ctycty2@qq.com>
2014-10-28 17:18:35 +0800

    搭框架
    搭框架

--- eos/index.php
+++ eos/index.php
@@ -1,13 +1,50 @@
 <?php
-class A1
+require_once("../myFramework/framework.php"); //class A
+class A1 extends A
 {
+	public function model0($a)
+	{
+		
+	}
+	public function view0($a)
+	{
+		return $this->table($a);
+	}
+	public function controler0()
+	{
+	
+	}
+	public function viewLayoutHeader()
+	{
+		$a='';
+		$a.="<br /><br /><br /><br /><br /><br /><br /><br />";
+		$a.="<a href='index.php'>ึ๗าณ</a><br /><br />";
+		return $this->style($a);
+	}
+	public function viewLayoutContent()
+	{
+	
+	}
+	
+	public function sessionCheck()
+	{
+		$systemName='';
+		$userId='';
+		$userName='';
+		return ;
+	}	
 	public function run()
 	{
-		$a="test";
-		echo $a;
-		return $a;
+		$a="";
+		$a.=$this->viewLayoutHeader();
+		//$a="test";
+		$a1=array(array(1,2),array(3,4,5));
+		//echo $this->table($a1);
+		echo  $a.$this->view0($a1);
+		//return $a;
 	}	
 }
+
 
 $a=new A1;
 $a->run();
--- myFramework/framework.php
+++ myFramework/framework.php
@@ -1,3 +1,39 @@
 <?php
-
+class A
+{
+	public function style($content){return '<p style="text-align:center;">'.$content.'</p>';}
+	public function table($a,$align="center")
+	{
+		$rr=count($a);
+		$rc=count($a[0]);
+		for($i=1;$i<$rr;$i++)
+			if($rc<count($a[$i]))
+				$rc=count($a[$i]);
+		for($i=0;$i<$rr;$i++)
+			for($j=0;$j<$rc;$j++)
+				if(!isset($a[$i][$j]))
+					$a[$i][$j]='-';
+		$b='<table border="1" style="text-align: '.$align.'; margin-left: auto; margin-right: auto;">';
+		$b=$b."<tr>";
+		for($i=0;$i<$rc;$i++)
+			$b=$b."<th>".$a[0][$i]."</th>";
+		$b=$b."</tr>";
+		for($j=1;$j<$rr;$j++) 
+		{
+			$b=$b."<tr>";
+			for($i=0;$i<$rc;$i++)
+			{
+				if(!isset($a[$j][$i]))
+					$a[$j][$i]="-";
+				elseif(trim($a[$j][$i])=='')
+					$a[$j][$i]="-";
+				$b=$b."<td>".$a[$j][$i]."</td>";
+			}
+			$b=$b."</tr>";
+		}
+		$b=$b."</table>";
+		$b=$this->style($b);
+		return $b;
+	}
+}
 ?>
